f593448e205525642e6da5a7b763345ffed11ecd
max vagner <max.vagner@sympli.com>
2021-03-11 13:45:59 +1100

    Setup hosted service

--- SuperTool/Program.cs
+++ SuperTool/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace SuperTool
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var host = CreateHostBuilder(args).Build();
+            await host.StartAsync();
+            await host.StopAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                       .ConfigureServices((hostContext, services) =>
+                       {
+                           services.AddHostedService<SuperToolHostedService>();
+
+                           // turn off the startup messages logged that we won't be using
+                           services.PostConfigure<ConsoleLifetimeOptions>(options => options.SuppressStatusMessages = true);
+                       });
         }
     }
 }
---
+++ SuperTool/SuperToolHostedService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SuperTool
+{
+    public class SuperToolHostedService : IHostedService
+    {
+        private ILogger<SuperToolHostedService> _logger { get; }
+
+        public SuperToolHostedService(ILogger<SuperToolHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Hi from the command line!");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Bye from the command line!");
+            return Task.CompletedTask;
+        }
+    }
+}

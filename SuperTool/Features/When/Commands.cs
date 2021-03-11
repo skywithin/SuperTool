1f7bdd55303e7a46eaf0bfdd55008906b2b1818c
max vagner <max.vagner@sympli.com>
2021-03-11 14:20:11 +1100

    Implement command line arguments

---
+++ SuperTool/Features/When/Commands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using MediatR;
+
+namespace SuperTool.Features.When
+{
+    public static class Commands
+    {
+        [Command(Name = "when", Description = "Get the current time")]
+        public class WhenCommand
+        {
+            public async Task OnExecute(IMediator mediator, IConsole console)
+            {
+                var response = await mediator.Send(new When.Query());
+                console.WriteLine($"Current time: {response.CurrentTime:O}");
+            }
+        }
+    }
+}
